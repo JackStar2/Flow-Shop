91a44933c2e8a471914bd379609aa4a7d7396ef1
supasentai <163082431+supasentai@users.noreply.github.com>
2024-10-09 22:04:02 +0700

    Add files via upload

---
+++ Discount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Discount
+{
+    public string Discount_ID { get; set; }
+    public string Discount_Type { get; set; }
+    public string Discount_value { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string ApplicableProduct { get; set; }
+
+    public Discount(string discount_ID, string discount_Type, string discount_value, DateTime startDate, DateTime endDate, string applicableProduct)
+    {
+        Discount_ID = discount_ID;
+        Discount_Type = discount_Type;
+        Discount_value = discount_value;
+        StartDate = startDate;
+        EndDate = endDate;
+        ApplicableProduct = applicableProduct;
+
+    }
+}
---
+++ transfer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_cuoi_ki
+{
+    public class Transfer
+    {
+        private int TransferID;
+        private DateTime Date;
+        private int CashierID;
+        private string PaymentMethod;
+        private decimal CustomerMoney;
+        private int CustomerID;
+        List<Product> ListItem = new List<Product>();
+
+        public int Transfer_ID { get => TransferID; set => TransferID = value; }
+        public DateTime Date_ { get => Date; set => Date = value; }
+        public int Cashier_ID { get => CashierID; set => CashierID = value; }
+        public string Payment_Method { get => PaymentMethod; set => PaymentMethod = value; }
+        public decimal Customer_Money { get => CustomerMoney; set => CustomerMoney = value; }
+        public int Customer_ID { get => CustomerID; set => CustomerID = value; }
+        public List<Product> List_Item { get => ListItem; set => ListItem = value; }
+
+        public Transfer(int transferID, DateTime date, int cashierID, string paymentMethod, decimal customerMoney, int customerID, List<Product> listItem)
+        {
+            TransferID = transferID;
+            Date = date;
+            CashierID = cashierID;
+            PaymentMethod = paymentMethod;
+            CustomerMoney = customerMoney;
+            CustomerID = customerID;
+            ListItem = listItem;
+        }
+
+        public decimal Total_Price()
+        {
+            decimal total = 0;
+            for (int i = 0; i < List_Item.Count; i++)
+            {
+                total += List_Item[i].ProductPrice * List_Item[i].ProductQuantity;
+            }
+            return total;
+        }
+
+        public decimal Exchange()
+        {
+            decimal total = Total_Price();
+            decimal exchange = Customer_Money - total;
+
+            if (exchange < 0)
+            {
+                Console.WriteLine("Số tiền không đủ để thanh toán.");
+                return 0;
+            }
+
+            return exchange;
+        }
+
+        public void Add_Item(Product product)
+        {
+            List_Item.Add(product);
+        }
+
+        public void Remove_Item(Product product)
+        {
+            List_Item.Remove(product);
+        }
+    }
+}
+
+
+   
