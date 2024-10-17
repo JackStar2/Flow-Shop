712b0555a8d7211a3a0caff9488aa77a5c439662
supasentai <163082431+supasentai@users.noreply.github.com>
2024-10-17 16:09:35 +0700

    Add files via upload

---
+++ list_Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+
+[Serializable]
+public class list_Customer : ISerializable
+{
+    public List<Customer> Customers { get; set; } = new List<Customer>();
+
+    public list_Customer() { }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue("Customers", Customers);
+    }
+
+    public list_Customer(SerializationInfo info, StreamingContext context)
+    {
+        Customers = (List<Customer>)info.GetValue("Customers", typeof(List<Customer>));
+    }
+
+    public void Add(Customer item)
+    {
+        Customers.Add(item);
+    }
+}
---
+++ list_Staff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class list_Staff : ISerializable
+{
+    public List<Staff> Staffs { get; set; } = new List<Staff>();
+    public list_Staff() { }
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        info.AddValue("Staffs", Staffs);
+    }
+
+    public list_Staff(SerializationInfo info, StreamingContext context)
+    {
+        Staffs = (List<Staff>)info.GetValue("Staffs", typeof(List<Staff>));
+    }
+
+    public void Add(Staff item)
+    {
+        Staffs.Add(item);
+    }
+}
